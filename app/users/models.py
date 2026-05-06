d4ed8d6a75d30e39065206bf228bbdc76c8de2af
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-06 13:38:07 -0600

    feat: implementar modelo sqlalchemy de usuarios

---
+++ app/users/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    username: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    role: Mapped[str] =   mapped_column(String(20), default="user", nullable=False)
+    disabled: Mapped[bool] = mapped_column(default=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
