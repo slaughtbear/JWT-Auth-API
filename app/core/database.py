b76ec095352972b38314efdc14c0a821d4518aea
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-06 13:34:56 -0600

    chore: agregar clase base para modelos de sqlalchemy

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.configuration import settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
 
 db_url = f"mysql+aiomysql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 engine = create_async_engine(db_url)
 async_session_local = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 async def get_session():
