4a80cfa0fdcbbf1a638c2709171b61db05c5f54d
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-07 16:26:20 -0600

    feat: actualizar valores por defecto en campos timestamps de la tabla users

--- app/users/models.py
+++ app/users/models.py
@@ -14,5 +14,5 @@ class User(Base):
     role: Mapped[str] =   mapped_column(String(20), default="user", nullable=False)
     disabled: Mapped[bool] = mapped_column(default=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime | None] = mapped_column(server_default=None, onupdate=func.now())
