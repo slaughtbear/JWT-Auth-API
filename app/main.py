a6943d7e62b738c26dfc3fb7075e296e4f6cea2d
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-07 16:09:51 -0600

    feat: agregar router de usuarios en la app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+from app.users.router import router as users_router
+
 
 app = FastAPI(
     title="JWT Auth API",
     version="1.0"
+)
+
+
+app.include_router(
+    router = users_router,
+    prefix = "/api/v1/admin/users",
+    tags = ["Users"]
 )
