249f0147f4644641be0d4bfbb7996b3e170ca5c9
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-07 19:18:14 -0600

    feat(users): implementar lectura de usuarios

--- app/users/repository.py
+++ app/users/repository.py
@@ -1,11 +1,18 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.users.schemas import UserDB
 from app.users.models import User
 
 
-async def create_user(user_data: dict, db: AsyncSession) -> UserDB:
+async def create_user(user_data: dict, db: AsyncSession) -> User:
     user_model = User(**user_data)
     db.add(user_model)
     await db.commit()
     await db.refresh(user_model)
     return user_model
+
+
+async def read_users(db: AsyncSession) -> list[User]:
+    statement = select(User)
+    result = await db.execute(statement)
+    users = result.scalars().all()
+    return users
--- app/users/router.py
+++ app/users/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_session
-from app.users.schemas import UserCreate
+from app.users.schemas import UserCreate, UserResponse
 from app.users import repository
 
 
@@ -12,3 +12,8 @@ router = APIRouter()
 @router.post("/")
 async def create_user(user: UserCreate, db: AsyncSession = Depends(get_session)):
     return await repository.create_user(user.model_dump(by_alias=True), db)
+
+
+@router.get("/", response_model=list[UserResponse])
+async def read_users(db: AsyncSession = Depends(get_session)):
+    return await repository.read_users(db)
