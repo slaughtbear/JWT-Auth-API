df01babf18230d1bbb5a4cc3aae5526a4a348488
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-08 11:27:30 -0600

    feat(users): agregar función para buscar a un usuario por id y otra para actualización

--- app/users/repository.py
+++ app/users/repository.py
@@ -3,6 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.users.models import User
 
 
+async def search_user_by_id(id: int, db: AsyncSession) -> User | None:
+    result = await db.execute(select(User).where(User.id == id))
+    user = result.scalars().first()
+    return user
+
+
 async def create_user(user_data: dict, db: AsyncSession) -> User:
     user_model = User(**user_data)
     db.add(user_model)
@@ -16,3 +22,12 @@ async def read_users(db: AsyncSession) -> list[User]:
     result = await db.execute(statement)
     users = result.scalars().all()
     return users
+
+
+async def update_user(user_data: dict, db_user: User, db: AsyncSession) -> User:
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
