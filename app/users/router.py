cf25a9bcd59426d8df3142a22a69f0a632412a24
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-08 19:42:05 -0600

    feat(users): proteger endpoints disponibles sólo para usuarios con rol de administrador

--- app/users/router.py
+++ app/users/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.auth.dependencies import check_admin
+
 from app.core.database import get_session
 
 from app.users import repository
@@ -12,20 +14,20 @@ router = APIRouter()
 
 
 @router.post("/", response_model=UserResponse)
-async def create_user(user_data: UserCreate, db: AsyncSession = Depends(get_session)):
+async def create_user(user_data: UserCreate, db: AsyncSession = Depends(get_session), admin = Depends(check_admin)):
     return await service.create_user(user_data, db)
 
 
 @router.get("/", response_model=list[UserResponse])
-async def read_users(db: AsyncSession = Depends(get_session)):
+async def read_users(db: AsyncSession = Depends(get_session), admin = Depends(check_admin)):
     return await repository.read_users(db)
 
 
 @router.patch("/{id}", response_model=UserResponse)
-async def update_user(id: int, user_data: UserUpdate, db: AsyncSession = Depends(get_session)):
+async def update_user(id: int, user_data: UserUpdate, db: AsyncSession = Depends(get_session), admin = Depends(check_admin)):
     return await service.update_user_by_id(id, user_data, db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(id: int, db: AsyncSession = Depends(get_session)) -> None:
+async def delete_user(id: int, db: AsyncSession = Depends(get_session), admin = Depends(check_admin)) -> None:
     await service.delete_user_by_id(id, db)
