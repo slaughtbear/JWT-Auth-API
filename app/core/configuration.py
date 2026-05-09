748b00cd78fd9f227be423210aae979a36636d1c
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-08 19:40:33 -0600

    chore: agregar variables de entorno para creación de tokens jwt

--- app/core/configuration.py
+++ app/core/configuration.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     db_port: str
     db_name: str
 
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
