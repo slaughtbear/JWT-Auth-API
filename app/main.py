03be020b1d0b77bbf10f28670a6ca55cfa4b0cd0
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-08 19:39:16 -0600

    feat: agregar router de autenticación en la app

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 
+from app.auth.router import router as auth_router
 from app.users.router import router as users_router
 
 
 app = FastAPI(
     title="JWT Auth API",
     version="1.0"
+)
+
+
+app.include_router(
+    router = auth_router,
+    prefix = "/api/v1/auth",
+    tags = ["Authentication"]
 )
 
 
