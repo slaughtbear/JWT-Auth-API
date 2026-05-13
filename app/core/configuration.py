39f1a6c712d1078ac0323d481eb543eb42c3d039
slaughtbear <ivan.aguirre1603@gmail.com>
2026-05-13 12:55:23 -0600

    feat: agregar middleware de cors para permitir al frontend realizar peticiones al backend

--- app/core/configuration.py
+++ app/core/configuration.py
@@ -12,6 +12,8 @@ class Settings(BaseSettings):
     algorithm: str
     access_token_expire_minutes: int
 
+    frontend_url: str
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth_router
 from app.users.router import router as users_router
+
+from app.core.configuration import settings
 
 
 app = FastAPI(
     title="JWT Auth API",
     version="1.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = [settings.frontend_url],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
 )
 
 
